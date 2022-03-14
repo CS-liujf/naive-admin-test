2406ecec17203f925d981e393229396893a9db08
Fly <1756339062@qq.com>
2022-03-14 09:36:13 +0800

    配置vite，详情请见配置文件 支持自动引入所需的naive ui组件、支持别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 下面这两行引入是为了自动解析模板中的使用到的组件，并导入组件
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
 
+
+function pathResolve(dir: string): string {
+  return resolve(__dirname, dir)
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
+  resolve: {
+    // 设置别名
+    alias: [
+      { find: '@', replacement: pathResolve('src') }
+    ]
+  },
+  server: {
+    open: true
+    //如果跨域，在此配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://your.url'
+    //   }
+    // }
+  }
 })
