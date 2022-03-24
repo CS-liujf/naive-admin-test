594610937c9c9b532106d1cbd43666b749b82feb
Fly <1756339062@qq.com>
2022-03-24 20:13:28 +0800

    给vite新增插件，支持往index.html里注入内容

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 // 下面这两行引入是为了自动解析模板中的使用到的组件，并导入组件
 import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import { resolve } from 'path'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+import {resolve} from 'path'
 // svg 组件
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
+// 下面这个插件可以给index.html注入内容
+import {createHtmlPlugin} from "vite-plugin-html";
 
 
 function pathResolve(dir: string): string {
   return resolve(__dirname, dir)
 }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -21,13 +24,21 @@ export default defineConfig({
     createSvgIconsPlugin({
       iconDirs: [pathResolve('src/icons')],
       symbolId: 'icon-[dir]-[name]'
+    }),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          //效果查看index.html中的<title>
+          title: 'index'
+        }
+      }
     })
   ],
   resolve: {
     // 设置别名
     alias: [
-      { find: '@', replacement: pathResolve('src') },
-      { find: 'store', replacement: pathResolve('src') }
+      {find: '@', replacement: pathResolve('src')},
+      {find: 'store', replacement: pathResolve('src')}
     ]
   },
   server: {
