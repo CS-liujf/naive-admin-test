400f76f5bcd7288a723dffaecba5d053fc0a4195
Fly <1756339062@qq.com>
2022-03-24 08:53:09 +0800

    layout中新增侧边栏组件SideBar

---
+++ src/layout/components/SideBar/SideBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <n-layout-sider content-style="padding: 24px;" :collapsed=isCollapsed>
+    海淀桥
+  </n-layout-sider>
+</template>
+
+<script lang="ts" setup>
+import isCollapsed from "@/layout/components/helper/"
+</script>
+
+<style scoped>
+
+</style>
