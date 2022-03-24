0abf4b5eeb772cfbb181e29904bb2387af29d971
Fly <1756339062@qq.com>
2022-03-24 08:51:13 +0800

    给header新增折叠侧边栏组件

---
+++ src/layout/components/header/components/HeaderCollapse.vue
@@ -0,0 +1,28 @@
+<template>
+<!--  <n-tooltip placement="right" trigger="hover">-->
+<!--    <template #trigger>-->
+      <HoverContainer @click="test" style="padding: 0 10px">
+        <SvgIcon v-if="!isCollapsed" name="收起" size="1.6em"></SvgIcon>
+        <SvgIcon v-else name="展开" size="1.6em"></SvgIcon>
+      </HoverContainer>
+<!--    </template>-->
+<!--    <span>{{message}}</span>-->
+<!--  </n-tooltip>-->
+</template>
+
+<script lang="ts" setup>
+import {computed, ref} from "vue"
+import SvgIcon from "@/components/SvgIcon/index.vue"
+import HoverContainer from "@/components/HoverContainer/HoverContainer.vue"
+import isCollapsed from "@/layout/components/helper/"
+// const isCollapsed=ref(true)
+function test(){
+  isCollapsed.value=!isCollapsed.value
+  // console.log(isCollapsed.value)
+}
+const message=computed(()=>{
+  return isCollapsed.value? '展开侧边栏':'收起侧边栏'
+})
+</script>
+<style scoped>
+</style>
