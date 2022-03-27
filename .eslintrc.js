e061a03b28fc103a08d168ee7601cf37c8fc15d5
Fly <1756339062@qq.com>
2022-03-27 21:48:27 +0800

    改用standard-with-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:vue/vue3-recommended',
-    'airbnb-base',
+    'standard-with-typescript'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
     'vue',
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
-  rules: {
-    // 如下规则用于重写airbnb中必须加 file extension的规定
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-  },
+  rules: { },
   settings: {
     // 安装eslint-import-resolver-typescript后在此配置import/resolver，即可读取项目根目录下tsconfig.json中的路径别名设置
     'import/resolver': {
-      typescript: {},
-    },
-  },
-};
+      typescript: {}
+    }
+  }
+}
