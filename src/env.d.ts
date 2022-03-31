cfa4e8a2b3e9f04b12e016d2571e40f504c65b78
Fly <1756339062@qq.com>
2022-03-31 12:50:43 +0800

    添加env环境变量

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,6 +10,12 @@ declare module '*.vue' {
       $notification: NotificationProviderInst,
       $loadingBar: LoadingBarProviderInst
     }
+    //这里必须增加该接口，否则编译器认为环境变量不存在于ImportMeta类型中
+    interface ImportMetaEnv {
+      VITE_APP_TITLE: string,
+      VITE_APP_BASEURL: string,
+      [propName: string]: number | string | boolean | Array<string>
+    }
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
