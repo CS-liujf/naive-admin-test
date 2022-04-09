7d47ce4583a79737547cf48bc2a17bb43ce61b9d
Fly <1756339062@qq.com>
2022-04-09 19:55:48 +0800

    修改了白天/夜间模式切换按钮

--- src/components/ModeSwitch/ModeSwitch.vue
+++ src/components/ModeSwitch/ModeSwitch.vue
@@ -1,22 +1,24 @@
 <template>
-  <n-tooltip placement="bottom" trigger="hover">
-    <template #trigger>
-      <div @click="switchMode()" class="svg-container">
-        <SvgIcon v-if="isDark" name="太阳" size="26"></SvgIcon>
-        <SvgIcon v-else name="月亮" size="28"></SvgIcon>
-      </div>
-    </template>
-    <span>{{ message }}</span>
-  </n-tooltip>
+  <div
+    class="svg-container"
+    @click="switchMode()"
+  >
+    <SvgIcon
+      v-if="isDark"
+      name="太阳"
+      size="26"
+    />
+    <SvgIcon
+      v-else
+      name="月亮"
+      size="28"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import SvgIcon from "../SvgIcon/index.vue";
-import {isDark, switchMode} from "@/util/switchMode";
-import {computed} from "vue";
-const message = computed(() => {
-  return isDark.value ? '切换白天模式' : '切换夜间模式'
-})
+import { isDark, switchMode } from '@/util/switchMode';
+import SvgIcon from '../SvgIcon/index.vue';
 </script>
 
 <style scoped>
--- src/layout/components/header/components/HeaderModeSwitch.vue
+++ src/layout/components/header/components/HeaderModeSwitch.vue
@@ -1,17 +1,25 @@
 <template>
-  <HoverContainer class="switch-container">
-    <ModeSwitch />
-  </HoverContainer>
+  <n-tooltip
+    placement="bottom"
+    trigger="hover"
+  >
+    <template #trigger>
+      <HoverContainer style="padding:0 2px">
+        <ModeSwitch />
+      </HoverContainer>
+    </template>
+    <span>{{ message }}</span>
+  </n-tooltip>
 </template>
 
 <script lang="ts" setup>
 import HoverContainer from '@/components/HoverContainer/HoverContainer.vue';
 import ModeSwitch from '@/components/ModeSwitch/ModeSwitch.vue';
+import { isDark } from '@/util/switchMode';
+import { computed } from 'vue';
+
+const message = computed(() => (isDark.value ? '切换白天模式' : '切换夜间模式'));
 </script>
 
 <style scoped>
-.switch-container {
-  /*width: 40px;*/
-  padding: 0 4px;
-}
 </style>
