b4d820441bf755d5f60d4fc33a89891d1db837e3
Fly <1756339062@qq.com>
2023-01-14 19:42:46 +0800

    eslint format

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 // 下面这两行引入是为了自动解析模板中的使用到的组件，并导入组件
-import Components from 'unplugin-vue-components/vite'
-import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
-import {resolve} from 'path'
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import { resolve } from 'path';
 // svg 组件
-import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 // 下面这个插件可以给index.html注入内容
-import {createHtmlPlugin} from "vite-plugin-html";
-//下面这个插件用于可视化打包后各个资源占用大小
-import { visualizer } from "rollup-plugin-visualizer";
+import { createHtmlPlugin } from 'vite-plugin-html';
+// 下面这个插件用于可视化打包后各个资源占用大小
+import { visualizer } from 'rollup-plugin-visualizer';
 
 function pathResolve(dir: string): string {
-  return resolve(__dirname, dir)
+  return resolve(__dirname, dir);
 }
 
 // https://vitejs.dev/config/
@@ -20,40 +20,40 @@ export default defineConfig({
   plugins: [
     vue(),
     Components({
-      resolvers: [NaiveUiResolver()]
+      resolvers: [NaiveUiResolver()],
     }),
     createSvgIconsPlugin({
       iconDirs: [pathResolve('src/icons')],
-      symbolId: 'icon-[dir]-[name]'
+      symbolId: 'icon-[dir]-[name]',
     }),
     createHtmlPlugin({
       inject: {
         data: {
-          //效果查看index.html中的<title>
-          title: 'index'
-        }
-      }
+          // 效果查看index.html中的<title>
+          title: 'index',
+        },
+      },
     }),
     visualizer({
       // npm run dev后会自动生成该html于项目根目录
       filename: '打包分析.html',
-      title: '可视化打包分析'
-    })
+      title: '可视化打包分析',
+    }),
   ],
   resolve: {
     // 设置别名
     alias: [
-      {find: '@', replacement: pathResolve('src')},
-      {find: 'store', replacement: pathResolve('src')}
-    ]
+      { find: '@', replacement: pathResolve('src') },
+      { find: 'store', replacement: pathResolve('src') },
+    ],
   },
   server: {
-    open: true
-    //如果跨域，在此配置
+    open: true,
+    // 如果跨域，在此配置
     // proxy: {
     //   '/api': {
     //     target: 'https://your.url'
     //   }
     // }
-  }
-})
+  },
+});
