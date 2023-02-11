c7957c257e1ca9ce6dd2880f8f104c32d00c4f9d
Fly <1756339062@qq.com>
2023-02-11 12:54:18 +0800

    fix: vite的有些type声明无法获取 如import.meta的globEager函数就无法识别

--- mock/mockProdServer.ts
+++ mock/mockProdServer.ts
@@ -1,6 +1,8 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
 
 const apiModules:any[] = [];
+// 注意tsconfig.json的compilerOptions里需要添加上"types": ["vite/client"]
+// 否则无法识别globEager函数
 const files = import.meta.globEager('./**/*.ts');
 Object.keys(files).forEach((key) => {
   // 可根据文件名判断哪些ts文件忽略
