d24e56507527bd74afc87786a704978bdd917062
Fly <1756339062@qq.com>
2023-02-16 09:22:49 +0800

    fix: 某些组件上路由过渡动画不生效 组件必须有一个根标签，否则路由过渡动画无法对其应用

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    {{ a }}
+    <div>
+      {{ a }}
+    </div>
+    <n-button type="primary">
+      你好
+    </n-button>
+    <n-card title="带封面的卡片">
+      <template #cover>
+        <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
+      </template>
+      卡片内容
+    </n-card>
+    <a href="#/a">跳转</a>
   </div>
-  <n-button type="primary">
-    你好
-  </n-button>
-  <n-card title="带封面的卡片">
-    <template #cover>
-      <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
-    </template>
-    卡片内容
-  </n-card>
-  <a href="#/a">跳转</a>
 </template>
 
 <script lang="ts" setup>
