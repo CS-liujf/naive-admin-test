cd740949785fb15754674fd7567118357f7261d9
Fly <1756339062@qq.com>
2023-02-21 12:44:13 +0800

    fix: 侧边栏变动时图表大小自适应

--- src/views/user-center/components/HeatMap.vue
+++ src/views/user-center/components/HeatMap.vue
@@ -101,11 +101,34 @@ const renderChart = (element:HTMLElement) => {
   myChart = echarts.init(element, chartTheme.value);
   myChart.setOption(option);
 };
+
+function debounce(fun:Function, delay = 400) {
+  // 定时器容器
+  let timer = null;
+  // 返回一个函数
+  return (...args:any[]) => {
+    // 清除定时器  可以保证该函数如果多次连续单位400ms毫秒内调用 ，只执行一次
+    if (timer) clearInterval(timer);
+    // 定时器
+    timer = setTimeout(() => {
+      fun.call(this, ...args);
+    }, delay);
+  };
+}
+
+let resizeOb:ResizeObserver;
 onMounted(() => {
   renderChart(chartDom.value!);
-  window.addEventListener('resize', () => {
-    myChart.resize();
-  });
+  resizeOb = new ResizeObserver(
+    debounce(() => {
+      // 如果容器里有多个图表就可以如下调用
+      // entries.forEach((entry) => {
+      //   echarts.getInstanceByDom(entry.target as unknown as HTMLElement)?.resize();
+      // });
+      myChart?.resize();
+    }, 14),
+  );
+  resizeOb.observe(chartDom.value!);
 });
 
 watch(isDark, () => { renderChart(chartDom.value!); });
