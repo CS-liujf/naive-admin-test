a191e42c91aa42844ff54c9afbe74cf0058ce7c6
Fly <1756339062@qq.com>
2023-02-22 22:24:52 +0800

    feat: 活动记录可以触发父组件方法

--- src/views/user-center/UserCenter.vue
+++ src/views/user-center/UserCenter.vue
@@ -2,16 +2,16 @@
   <div>
     <n-grid
       cols="4"
-      :x-gap="16"
+      :x-gap="18"
       item-responsive
     >
       <n-grid-item :span="3">
         <n-space
-          :size="16"
+          :size="20"
           vertical
         >
           <heat-map />
-          <history-log />
+          <history-log @select-year="selectYear" />
         </n-space>
       </n-grid-item>
       <n-grid-item :span="1">
@@ -20,6 +20,9 @@
           vertical
         >
           <avatar-profile />
+          <n-button @click="selectYear">
+            click
+          </n-button>
         </n-space>
       </n-grid-item>
     </n-grid>
@@ -30,6 +33,10 @@
 import HeatMap from './components/HeatMap.vue';
 import AvatarProfile from './components/AvatarProfile.vue';
 import HistoryLog from './components/HistoryLog.vue';
+
+const selectYear = (year:string) => {
+  window.$message.success(year);
+};
 </script>
 
 <style scoped>
--- src/views/user-center/components/HistoryLog.vue
+++ src/views/user-center/components/HistoryLog.vue
@@ -1,10 +1,52 @@
 <template>
   <n-card title="历史活动">
-    fsdf
+    <!--    <n-space :size="50">-->
+    <n-grid
+      cols="5"
+      x-gap="30"
+      item-responsive
+    >
+      <n-grid-item :span="1">
+        <n-menu
+          :options="options"
+          default-value="1"
+          @update:value="selectYear"
+        />
+      </n-grid-item>
+      <n-grid-item :span="4">
+        <div>
+          fsdfsdffsadfsdf
+        </div>
+      </n-grid-item>
+    </n-grid>
+    <!--    </n-space>-->
   </n-card>
 </template>
 <script lang="ts" setup>
+import { reactive, defineEmits } from 'vue';
+import type { MenuOption } from 'naive-ui';
 
+const options = reactive<MenuOption[]>([
+  {
+    label: '2023年',
+    key: '1',
+  },
+  {
+    label: '2022年',
+    key: '2',
+  },
+  {
+    label: '2021年',
+    key: '3',
+  },
+]);
+
+const emit = defineEmits(['selectYear']);
+const selectYear = async (key:string, item:MenuOption) => {
+  console.log(item);
+  emit('selectYear', item.label);
+};
 </script>
+
 <style scoped>
 </style>
