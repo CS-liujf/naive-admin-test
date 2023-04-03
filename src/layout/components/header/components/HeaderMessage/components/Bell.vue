cbe22858fe1a721dc8b395458928b78823b6e45b
Fly <1756339062@qq.com>
2023-04-03 10:00:14 +0800

    feat: 将header中消息铃铛抽离为组件

---
+++ src/layout/components/header/components/HeaderMessage/components/Bell.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <n-tooltip
+      placement="bottom"
+      trigger="hover"
+    >
+      <template #trigger>
+        <HoverContainer style="padding:0 2px;width: 40px">
+          <n-badge
+            :value="10"
+            :max="15"
+            :offset="[4,0]"
+          >
+            <svg-icon
+              name="消息"
+              size="21"
+              :color="iconColor"
+            />
+          </n-badge>
+        </HoverContainer>
+      </template>
+      <span>消息通知</span>
+    </n-tooltip>
+  </div>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { isDark } from '@/utils/switchMode';
+import HoverContainer from '@/components/HoverContainer/HoverContainer.vue';
+
+const iconColor = computed(() => (isDark.value ? '#e6e6e6' : '#515151'));
+</script>
+<style scoped>
+</style>
