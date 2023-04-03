01f2d33903e3d580720f16b325d3b7aaa3429bab
Fly <1756339062@qq.com>
2023-04-03 10:12:18 +0800

    refactor: 用v-bind重写MainContent组件的css

--- src/layout/components/MainContent/MainContent.vue
+++ src/layout/components/MainContent/MainContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-layout-content :class="className">
+  <n-layout-content class="content-container">
     <router-view v-slot="{Component,route}">
       <transition
         name="content"
@@ -17,21 +17,18 @@
 import { isDark } from '@/utils/switchMode';
 import { computed } from 'vue';
 
-const className = computed(() => (isDark.value ? 'night-content-container' : 'light-content-container'));
+const backgroundColor = computed(() => (isDark.value ? '#101014' : '#f6f9f8'));
 </script>
 
 <style scoped>
+/* 给transition组件提供动画的css */
+/* 不可删去！*/
 .content-enter-active{
   animation: fadeIn 0.3s;
 }
-.light-content-container{
+.content-container{
   padding: 18px 18px;
-  background-color:#f6f9f8;
-  min-height:calc(100vh - 54px);
-}
-.night-content-container{
-  padding: 18px 18px;
-  background-color:#101014;
+  background-color:v-bind(backgroundColor);
   min-height:calc(100vh - 54px);
 }
 </style>
